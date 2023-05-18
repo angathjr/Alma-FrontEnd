b1599b18f4327275833cdc80e6a945c2fd705009
Shanil-AV <shanilazhuvalappil@gmail.com>
2023-05-18 23:39:48 +0530

    changes in eventcontroller

--- lib/events/controllers/event_controller.dart
+++ lib/events/controllers/event_controller.dart
@@ -1,20 +1,43 @@
 import 'dart:developer';
-
 import 'package:alma/core/api_provider_no_auth.dart';
 import 'package:alma/events/models/event_model.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+import '../../auth/models/user.dart';
 
 class EventsController extends GetxController {
-  final ApiProviderNoAuth api = Get.find();
+ final TextEditingController companyNameController =TextEditingController();
+ final TextEditingController eventNameController =TextEditingController();
+ final TextEditingController eventTypeController =TextEditingController();
+ final TextEditingController eventDateController=TextEditingController();
+ final TextEditingController descriptionController=TextEditingController();
+ final TextEditingController endDateController =TextEditingController();
+ final TextEditingController lastDateToApplyController =TextEditingController();
+ final TextEditingController institutionNameController =TextEditingController();
+ final TextEditingController roleController =TextEditingController();
+ final TextEditingController eventDescriptionController =TextEditingController();
+
   var events = <EventModel>[].obs;
+  var jobs = <EventModel>[].obs;
+ 
+  final ApiProviderNoAuth api = Get.find();
+   final _storage = GetStorage();
+  late EventModel eventModel;
 
-  void fetchEvents() async {
-    final response = await api.getApi('/events/all');
-    // log(response.statusCode.toString());
-    // log(response.body);
+  void fetchJob ( )async
+{
+final response = await api.getApi('/events/job');
+log("guyguyS${response.body}");
 
-    List<EventModel> parsed = eventModelFromJson(response.body);
-    log("parses data${parsed[0].eventName}");
-    log(parsed[0].skillsRequired![0].name.toString());
-  }
+final parsed = eventModelFromJson(response.body);
+jobs.value=parsed;
+//print(jobs[0].eventName);
+log("shanilllll${jobs[0].eventName}");
+
 }
+
+}
+    
+   
