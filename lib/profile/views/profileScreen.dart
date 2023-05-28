966f7525b4e6fce8ff86b5f32daf922b71297313
Kishan <kishansajith2003@gmail.com>
2023-05-28 18:11:05 +0530

    profile page modified

--- lib/profile/views/profileScreen.dart
+++ lib/profile/views/profileScreen.dart
@@ -77,83 +77,102 @@ class ProfilePage extends StatelessWidget {
             ),
           ),
           SizedBox(
-            height: 0.02 * height,
+            height: 0.004 * height,
           ),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
+              SizedBox(
+                width: 0.05 * width,
+              ),
+              Expanded(
+                child: Container(
+                  height: 0.3 * height,
+                  width: 0.445 * width,
+                  decoration: BoxDecoration(
+                      color: Color(0xff292A36),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.edit),
+                      Text(
+                        'Edit Profile',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Helvatica',
+                          //fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 0.01 * width,
+              ),
               Container(
                 height: 0.3 * height,
-                width: 0.405 * width,
-                decoration: BoxDecoration(
-                    color: Color(0xff292A36),
-                    borderRadius: BorderRadius.circular(10)),
+                width: 0.445 * width,
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.edit),
-                    Text(
-                      'Edit Profile',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'Helvatica',
-                        //fontWeight: FontWeight.bold
+                    Expanded(
+                      child: Container(
+                        height: 0.148 * height,
+                        width: 0.445 * width,
+                        decoration: BoxDecoration(
+                            color: Color(0xff292A36),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.post_add),
+                            Text(
+                              'New Posts',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontFamily: 'Helvatica',
+                                //fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
+                    SizedBox(
+                      height: 0.004 * height,
+                    ),
+                    GestureDetector(
+                      onTap: () => authController.signout(),
+                      child: Expanded(
+                        child: Container(
+                          height: 0.148 * height,
+                          width: 0.445 * width,
+                          decoration: BoxDecoration(
+                              color: Color(0xff292A36),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.logout),
+                              Text(
+                                'Log Out',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontFamily: 'Helvatica',
+                                  //fontWeight: FontWeight.bold
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
                   ],
                 ),
               ),
-              Column(
-                children: [
-                  Container(
-                    height: 0.14 * height,
-                    width: 0.405 * width,
-                    decoration: BoxDecoration(
-                        color: Color(0xff292A36),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.post_add),
-                        Text(
-                          'New Posts',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontFamily: 'Helvatica',
-                            //fontWeight: FontWeight.bold
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 0.02 * height,
-                  ),
-                  GestureDetector(
-                    onTap: () => authController.signout(),
-                    child: Container(
-                      height: 0.14 * height,
-                      width: 0.405 * width,
-                      decoration: BoxDecoration(
-                          color: Color(0xff292A36),
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.logout),
-                          Text(
-                            'Log Out',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontFamily: 'Helvatica',
-                              //fontWeight: FontWeight.bold
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
+              SizedBox(
+                width: 0.05 * width,
               )
             ],
           ),
