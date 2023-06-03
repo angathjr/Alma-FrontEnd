a1d9a9facc95f20423e4008ccc0b0e88ecc0c67e
Shanil-AV <shanilazhuvalappil@gmail.com>
2023-06-04 00:12:08 +0530

    blaaaaa

--- lib/search/views/search_screen.dart
+++ lib/search/views/search_screen.dart
@@ -154,6 +154,7 @@ class SearchCard extends StatelessWidget {
                     child: ClipRRect(
                         borderRadius: BorderRadius.circular(15),
                         child: Image.network(
+                          
                           "${searchController.events[index].imgUrl}}",
                           fit: BoxFit.cover,
                         )),
@@ -163,28 +164,31 @@ class SearchCard extends StatelessWidget {
               Expanded(
                 flex: 5,
                 child: Container(
+                //  color: Colors.amber,
                   child: Column(children: [
                     Padding(
-                      padding: const EdgeInsets.fromLTRB(10, 30, 10, 10),
-                      child: Text(
-                        "${searchController.events[index].eventName}",
-                        style: const TextStyle(
-                            fontSize: 32,
-                            fontFamily: 'Helavtica',
-                            fontWeight: FontWeight.bold),
+                      padding: const EdgeInsets.fromLTRB(10,30,10,10),
+                      child: Text("${searchController.events[index].eventName}",
+                       style: const TextStyle(
+                                       fontSize: 32,
+                                       fontFamily: 'Helavtica',
+                                       fontWeight: FontWeight.bold
+                                     ),
                       ),
                     ),
+
+                    
                     Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        "${searchController.events[index].eventDescription}",
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 4,
-                        style: const TextStyle(
-                          fontSize: 12,
-                          fontFamily: 'Helavtica',
-                        ),
-                      ),
+                      child: Text("${searchController.events[index].eventDescription}",
+                       overflow: TextOverflow.ellipsis,
+                       maxLines: 4,
+                       style: const TextStyle(
+                                       fontSize: 12,
+                                       fontFamily: 'Helavtica',
+                                       
+                                     ),
+                                     ),
                     ),
                   ]),
                 ),
