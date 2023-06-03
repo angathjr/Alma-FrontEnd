9658250eb35c0bfcaca03cbc775b7572f5f4b7a4
angathjr <angathjr@gmail.com>
2023-06-03 21:20:05 +0530

    changed navabar

--- lib/Navbar/views/navbar.dart
+++ lib/Navbar/views/navbar.dart
@@ -1,11 +1,11 @@
 import 'package:alma/Navbar/controllers/navcontroller.dart';
 import 'package:alma/Post/views/post_screen.dart';
-import 'package:alma/core/constants.dart';
 import 'package:alma/home/views/feedScreen.dart';
 import 'package:alma/profile/views/profileScreen.dart';
 import 'package:alma/search/views/search_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
 
 import '../../eventCalendar/views/calendar_screen.dart';
 
@@ -66,34 +66,31 @@ class NavBarPage extends StatelessWidget {
                       children: [
                         buildNavIcons(
                             navHeight: navHeight,
-                            name: 'home',
-                            w: 0.40,
-                            h: 0.40,
-                            index: 0),
+                            width: width,
+                            index: 0,
+                            icon: const Icon(Iconsax.home)),
                         buildNavIcons(
                             navHeight: navHeight,
-                            name: 'search',
-                            w: 0.35,
-                            h: 0.35,
-                            index: 1),
+                            width: width,
+                            index: 1,
+                            icon: const Icon(Iconsax.search_normal_1)),
                         buildNavIcons(
                             navHeight: navHeight,
-                            name: 'add',
-                            w: 0.6,
-                            h: 0.6,
-                            index: 2),
+                            width: width,
+                            index: 2,
+                            icon: const Icon(
+                              Iconsax.add_square,
+                            )),
                         buildNavIcons(
                             navHeight: navHeight,
-                            name: 'calendar',
-                            w: 0.35,
-                            h: 0.35,
-                            index: 3),
+                            width: width,
+                            index: 3,
+                            icon: const Icon(Iconsax.calendar_search)),
                         buildNavIcons(
+                            width: width,
                             navHeight: navHeight,
-                            name: 'user',
-                            w: 0.35,
-                            h: 0.35,
-                            index: 4),
+                            index: 4,
+                            icon: const Icon(Iconsax.user)),
                       ],
                     ),
                   ),
@@ -104,45 +101,44 @@ class NavBarPage extends StatelessWidget {
         ));
   }
 
-  InkWell buildNavIcons(
+  Widget buildNavIcons(
       {required double navHeight,
-      required String name,
-      required double w,
-      required double h,
-      required int index}) {
-    return InkWell(
-      onTap: () {
-        navController.changePage(index, navController.controller);
-        navController.index.value = index;
-      },
-      child: Stack(
-        alignment: AlignmentDirectional.center,
-        children: [
-          Obx(
-            () => AnimatedContainer(
-              curve: Curves.linear,
-              duration: const Duration(milliseconds: 200),
-              width: navHeight * 0.7,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: (navController.index.value == index &&
-                          navController.index.value != 2)
-                      ? const Color(0xffD9D9D9).withOpacity(0.15)
-                      : Colors.transparent),
-            ),
-          ),
-          Container(
-            width: navHeight * w,
-            height: navHeight * h,
-            decoration: const BoxDecoration(
-                // color: Colors.blue,
+      required int index,
+      required Icon icon,
+      required double width}) {
+    return SizedBox(
+        width: navHeight,
+        height: navHeight,
+        child: Obx(() => IconButton(
+            color: Colors.grey.shade400,
+            enableFeedback: true,
+            splashRadius: 5,
+            disabledColor: Colors.red,
+            onPressed: () {
+              navController.changePage(index, navController.controller);
+              navController.index.value = index;
+            },
+            icon: Stack(
+              children: [
+                Center(child: icon),
+                // if (index == navController.index.value)
+                Center(
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.linear,
+                    width:
+                        (index == navController.index.value) ? width * 0.05 : 0,
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          width: 1,
+                          color: Colors.blueGrey,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-            child: Image.asset(
-              "assets/navbar/$name.png",
-            ),
-          ),
-        ],
-      ),
-    );
+              ],
+            ))));
   }
 }
