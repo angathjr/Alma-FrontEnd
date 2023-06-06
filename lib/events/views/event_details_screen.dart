f0c9c24f8be8388522dd52368d857487206b5a5d
Shanil-AV <shanilazhuvalappil@gmail.com>
2023-06-07 03:46:47 +0530

    some changes

--- lib/events/views/event_details_screen.dart
+++ lib/events/views/event_details_screen.dart
@@ -29,87 +29,94 @@ class EventDetailsScreen extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: width * 0.04),
         child: SingleChildScrollView(
-          child: Column(children: [
-            SizedBox(
-              height: height * 0.03,
-            ),
-            if (controller.selectedEvent.value.imgUrl != "")
-              Container(
-                // height: height * 0.58,
-                width: width,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: CachedNetworkImage(
-                      progressIndicatorBuilder:
-                          (context, url, downloadProgress) => Center(
-                        child: CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation(
-                                context.theme.disabledColor),
-                            value: downloadProgress.progress),
-                      ),
-                      imageUrl: "${controller.selectedEvent.value.imgUrl}",
-                      fit: BoxFit.cover,
-                    )),
+          child: 
+          Container(
+            child: Column(children: [
+              SizedBox(
+                height: height * 0.03,
               ),
-            Container(
-              constraints: BoxConstraints(
-                  maxHeight: (controller.selectedEvent.value.imgUrl != "")
-                      ? height * 0.12
-                      : height * .5),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: height * 0.03,
-                  ),
+              if (controller.selectedEvent.value.imgUrl != "")
+                Column(
+                  children: [
+                    Container(
+                      // height: height * 0.58,
+                      width: width,
+                      decoration: BoxDecoration(
+                       // color: Colors.amber,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: CachedNetworkImage(
+                            progressIndicatorBuilder:
+                                (context, url, downloadProgress) => Center(
+                              child: CircularProgressIndicator(
+                                  valueColor: AlwaysStoppedAnimation(
+                                      context.theme.disabledColor),
+                                  value: downloadProgress.progress),
+                            ),
+                            imageUrl: "${controller.selectedEvent.value.imgUrl}",
+                            fit: BoxFit.cover,
+                          )),
+                          
+                    ),
+                     Container(
+                constraints: BoxConstraints(
+                    maxHeight: (controller.selectedEvent.value.imgUrl != "")
+                        ? height * 0.12
+                        : height * .5),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: height * 0.03,
+                    ),
+                    Text(
+                      "${controller.selectedEvent.value.eventName}",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 3,
+                      style: const TextStyle(
+                          fontSize: 27,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    
+                  ],
+                ),
+              ),
+                  ],
+                ),
+               
+              
+              
+              Row(
+                children: const [
                   Text(
-                    "${controller.selectedEvent.value.eventName}",
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 3,
-                    style: const TextStyle(
-                        fontSize: 27,
+                    "Description:",
+                    style: TextStyle(
+                        fontSize: 20,
                         fontWeight: FontWeight.bold),
                   ),
-                  
                 ],
               ),
-            ),
-            Column(children:
-             [
-              // SizedBox(
-              //       height: height * 0.03,
-              //     ),
-                  Row(
-                    children: const [
-                      Text(
-                        "Description:",
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 10, 12, 0),
+                child: Container(
+                  width: width,
+                  child: Text(
+                    
+                      "${controller.selectedEvent.value.eventDescription}",
+                      softWrap: false,
+                      
+                            
+                           
+                      style: Constants.txtStyle().copyWith(fontSize: 16),
+                      // overflow: TextOverflow.ellipsis
+                      
                       ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(15, 10, 12, 0),
-                    child: Container(
-                      width: width,
-                      child: Text(
-                        
-                          "${controller.selectedEvent.value.eventDescription}",
-                          softWrap: false,
-                          
-                                
-                               
-                          style: Constants.txtStyle().copyWith(fontSize: 16),
-                          // overflow: TextOverflow.ellipsis
-                          
-                          ),
-                    ),
-                        
-                  ),],)
-          ]),
+                ),
+                    
+              )
+            ]),
+          ),
         ),
       ),
     );
