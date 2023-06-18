81d09717a9bcc58a6efaaf3be2299de0a413d4e2
Shanil-AV <shanilazhuvalappil@gmail.com>
2023-06-18 14:51:58 +0530

    alumni directry modified

--- lib/alumniDirectory/views/alumni_directory.dart.dart
+++ lib/alumniDirectory/views/alumni_directory.dart.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:alma/alumniDirectory/controllers/alumni_controller.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:feather_icons/feather_icons.dart';
@@ -6,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import '../../Post/views/event_type_selection_screen.dart';
 import '../../core/constants.dart';
 
 class AlumniDirectoryScreen extends StatelessWidget {
@@ -25,22 +28,171 @@ class AlumniDirectoryScreen extends StatelessWidget {
                   onPressed: () {
                     Get.back();
                   },
-                  icon:  Icon(Iconsax.arrow_left_2)),
-              title:  const Text(
+                  icon: Icon(Iconsax.arrow_left_2)),
+              title: const Text(
                 'Alumni Directory',
                 style: TextStyle(color: Colors.white),
               ),
-               actions: <Widget>[
-    IconButton(
-      icon: const Icon(
-        Iconsax.filter,
-        color: Colors.white,
-      ),
-      onPressed: () {
-       
-      },
-    )
-  ],
+              actions: <Widget>[
+                IconButton(
+                  icon: const Icon(
+                    Iconsax.filter,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    showModalBottomSheet(
+                      isScrollControlled: true,
+                      isDismissible: true,
+                      context: context,
+                      backgroundColor: Colors.transparent,
+                      useSafeArea: true,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(20))),
+                      builder: (context) => BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+                          child: Container(
+                            height: height * 0.2,
+                            width: width,
+                            decoration: BoxDecoration(
+                              color: Constants.cardColor(),
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20),
+                              ),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Expanded(
+                                  flex: 2,
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "Sort by ",
+                                      style: TextStyle(fontSize: width * 0.06),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 10,
+                                  child: Container(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: width * 0.08,
+                                          vertical: width * 0.05),
+                                      width: width,
+                                      child: Column(
+                                        children: [
+                                          Expanded(
+                                            flex: 7,
+                                            child: Row(
+                                              children: [
+                                                Expanded(
+                                                    child: GestureDetector(
+                                                  onTap: () {
+                                                    showDialog(
+                                                        context: context,
+                                                        builder: (BuildContext
+                                                                context) =>
+                                                            year);
+                                                  },
+                                                  child: Container(
+                                                      alignment:
+                                                          Alignment.center,
+                                                      decoration: BoxDecoration(
+                                                        color: Colors.white12,
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(20),
+                                                      ),
+                                                      height: height,
+                                                      child: Column(
+                                                        mainAxisAlignment:
+                                                            MainAxisAlignment
+                                                                .center,
+                                                        children: [
+                                                          Text(
+                                                            "Year",
+                                                            style: TextStyle(
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .w500,
+                                                                fontSize:
+                                                                    width *
+                                                                        0.05,
+                                                                color: Colors
+                                                                    .white),
+                                                          ),
+                                                          const Icon(
+                                                              Iconsax.calendar,
+                                                              color:
+                                                                  Colors.white)
+                                                        ],
+                                                      )),
+                                                )),
+                                                SizedBox(
+                                                  width: width * .03,
+                                                ),
+                                                Expanded(
+                                                    child: GestureDetector(
+                                                 onTap: () {
+                                                    showDialog(
+                                                    
+                                                        context: context,
+                                                        builder: (BuildContext
+                                                                context) =>
+                                                            department);
+                                                  },
+                                                  child: Container(
+                                                      alignment:
+                                                          Alignment.center,
+                                                      decoration: BoxDecoration(
+                                                        color: Colors.white12,
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(20),
+                                                      ),
+                                                      height: height,
+                                                      child: Column(
+                                                        mainAxisAlignment:
+                                                            MainAxisAlignment
+                                                                .center,
+                                                        children: [
+                                                          Text(
+                                                            "Department",
+                                                            style: TextStyle(
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .w500,
+                                                                fontSize:
+                                                                    width *
+                                                                        0.05,
+                                                                color: Colors
+                                                                    .white),
+                                                          ),
+                                                          const Icon(
+                                                              Iconsax
+                                                                  .courthouse,
+                                                              color:
+                                                                  Colors.white)
+                                                        ],
+                                                      )),
+                                                )),
+                                              ],
+                                            ),
+                                          ),
+                                        ],
+                                      )),
+                                )
+                              ],
+                            ),
+                          )),
+                    );
+                  },
+                )
+              ],
             ),
             SliverPadding(
                 padding: EdgeInsets.symmetric(horizontal: width * 0.04),
@@ -86,7 +238,6 @@ class AlumniDirectoryScreen extends StatelessWidget {
                                         curve: Curves.fastLinearToSlowEaseIn,
                                         duration:
                                             const Duration(milliseconds: 1500),
-                                            
                                         child: Container(
                                             width: width,
                                             height: height * .22,
@@ -174,7 +325,8 @@ class AlumniDirectoryScreen extends StatelessWidget {
                                                                 TextOverflow
                                                                     .ellipsis,
                                                             maxLines: 4,
-                                                            style: const TextStyle(
+                                                            style:
+                                                                const TextStyle(
                                                               fontSize: 14,
                                                             ),
                                                           ),
@@ -239,3 +391,115 @@ class AlumniDirectoryScreen extends StatelessWidget {
         ));
   }
 }
+
+//dialogue
+
+Dialog year = Dialog(
+  child: Container(
+    height: 150.0,
+    width: 360.0,
+    decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        //color: Constants.cardColor().withOpacity(0.7)
+        ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Text(
+            'Enter Batch',
+            style: TextStyle(color: Colors.white, fontSize: 22.0),
+          ),
+        ),
+        Container(
+          width: 250,
+          height: 40,
+          decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              color: Constants.cardColor().withOpacity(0.5)),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(left: 5),
+            ),
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+           width: 100,
+           height: 25,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.grey.withOpacity(0.5)),
+
+          child: Text(
+            "sort",
+            style: TextStyle(
+                fontWeight: FontWeight.w500, fontSize: 12, color: Colors.white),
+          ),
+        ),
+      ],
+    ),
+  ),
+);
+
+
+
+Dialog department = Dialog(
+  child: Container(
+    height: 150.0,
+    width: 360.0,
+    decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        //color: Constants.cardColor().withOpacity(0.7)
+        ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Text(
+            'Enter Department',
+            style: TextStyle(color: Colors.white, fontSize: 22.0),
+          ),
+        ),
+        Container(
+          width: 250,
+          height: 40,
+          decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              color: Constants.cardColor().withOpacity(0.5)),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(left: 5),
+            ),
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+           width: 100,
+           height: 25,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.grey.withOpacity(0.5)),
+
+          child: Text(
+            "Sort",
+            style: TextStyle(
+                fontWeight: FontWeight.w500, fontSize: 12, color: Colors.white),
+          ),
+        ),
+      ],
+    ),
+  ),
+);
