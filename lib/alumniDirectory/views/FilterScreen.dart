21860c6b5c2667c59875a2c58e51e685e4b4e0c7
Shanil-AV <shanilazhuvalappil@gmail.com>
2023-06-18 15:31:45 +0530

    small cahnge

---
+++ lib/alumniDirectory/views/FilterScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class FilterScreen extends StatelessWidget {
+   FilterScreen({
+   super.key,
+    required this.width,
+    required this.height,
+
+  
+  });
+   final double width;
+    final double height;
+  @override
+  Widget build(BuildContext context) {
+    return  Placeholder();
+  }
+}
